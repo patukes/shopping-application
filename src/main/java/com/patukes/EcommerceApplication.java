93ac7955429e23e774ca58f67c8147e46b2b9edb
pato <pato@gmail.com>
2020-07-10 18:47:27 +0200

    tables created succesfully

--- src/main/java/com/patukes/EcommerceApplication.java
+++ src/main/java/com/patukes/EcommerceApplication.java
@@ -1,11 +1,12 @@
 package com.patukes;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@EntityScan("com.patukes.model")
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
